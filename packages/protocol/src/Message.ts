6e32d9153c70d77d9ff97baf53ca0f79a832d673
Carlos Kieliszewski <carlos.kieliszewski@gmail.com>
2023-02-28 07:29:40 -0700

    Added nx except eclint

--- packages/protocol/src/Message.ts
+++ packages/protocol/src/Message.ts
@@ -1,6 +1,7 @@
 
 type MessageType = 
-  "comet_connect" | "plugin_connect";
+  "comet_connect" | "plugin_connect" |
+  "message_create" | "plugin_verify" | "plugin_verify_response";
 
 interface Message {
   type: MessageType;
